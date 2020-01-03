266dbe3c4fce906ca6acc6ebc1185e80f771eff8
shin <sukemayu5@gmail.com>
2020-01-03 14:57:10 +0000

    add task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-    validates :content, presence: true, text: { maximum: 255 }
-    validates :status, presence: true, text: { maximum: 255 }
     validates :content, presence: true,length: { maximum: 255 }
     validates :status, presence: true, length: { maximum: 10 }
 end

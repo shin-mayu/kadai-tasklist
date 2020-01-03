7141dab3752a2e5402e3601c85cf15c9b2dbe03e
shin <sukemayu5@gmail.com>
2020-01-03 02:49:17 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,35 +1,31 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
-      @tasks = Task.all
+      @task = Task.all
   end
-
   def show
       @task = Task.find(params[:id])
   end
-
   def new
       @task = Task.new
   end
-
   def create
       @task = Task.new(task_params)
-
-    if @task.save
+      
+      if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @Task
+      redirect_to @task
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
-    end
+    end  
   end
-
   def edit
       @task = Task.find(params[:id])
   end
-
   def update
        @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -38,19 +34,20 @@ class TasksController < ApplicationController
       render :edit
     end
   end
-
   def destroy
       @task = Task.find(params[:id])
-    @task.destroy
-
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+      @task.destroy
+      flash[:success] = 'Task は正常に削除されました'
+      redirect_to tasks_url
   end
-  
-   private
+end
 
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content)
+private
+
+def set_task
+    @task = Task.find(params[:id])
   end
+
+def task_params
+    params.require(:task).permit(:content, :status)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_083638) do
+ActiveRecord::Schema.define(version: 2020_01_03_023712) do
 
   create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_12_30_083638) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
